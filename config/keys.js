bfa736fc22a80a464f4f12fc1de0cca7f8c7ec59
masv <you@example.com>
2020-01-07 14:50:38 -0300

    fix

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-    //mongoURI: "mongodb+srv://auth-user:auth-pass@auth-9m6bh.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://auth-user:auth-pass@auth-9m6bh.mongodb.net/test?retryWrites=true&w=majority",
     //mongoURI: "mongodb://localhost/colocolo",
-    mongoURI : "mongodb+srv://<dbuser>:<dbpassword>@ds137220.mlab.com:37220/heroku_lw3l3cbw",
+    //mongoURI : "mongodb+srv://<dbuser>:<dbpassword>@ds137220.mlab.com:37220/heroku_lw3l3cbw",
     secretOrKey: "secret" 
   };
